4955fea4f2cdc999fa5d5922194662e968a9e06f
Vianka Rivera <viankalrivera@gmail.com>
2020-05-13 02:04:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all.self.save
+    @@all << self
   end
   def self.all
     @@all.each do |dog|
@@ -16,6 +16,9 @@ class Dog
     @@all.each do |dog|
       puts dog.name
     end
+  end
+  def save
+    @@all << self
   end
   def self.clear_all
     @@all.clear
